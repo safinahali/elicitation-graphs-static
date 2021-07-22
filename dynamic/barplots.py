a35fd3143d39edbcbe8c5e23a6499aa89a7a4440
Safinah Ali <safinahaali@gmail.com>
2021-07-22 16:35:41 -0400

    dynamic

--- dynamic/barplots.py
+++ dynamic/barplots.py
@@ -13,19 +13,26 @@ threshold_val_second = (3*a+b)/4
 fp_confusion = df.loc[df["threshold"] == threshold_val, "fp"]
 tp_confusion = df.loc[df["threshold"] == threshold_val, "tp"]
 fn_confusion = df.loc[df["threshold"] == threshold_val, "fn"]
+tn_confusion = df.loc[df["threshold"] == threshold_val, "tn"]
 
 fp_confusion_second = df.loc[df["threshold"] == threshold_val_second, "fp"]
 tp_confusion_second = df.loc[df["threshold"] == threshold_val_second, "tp"]
 fn_confusion_second = df.loc[df["threshold"] == threshold_val_second, "fn"]
+tn_confusion_second = df.loc[df["threshold"] == threshold_val_second, "tn"]
 
-x = ["Threshold = a", "Threshold = (3a+b)/4"]
 
+x = ["tp", "fp", "tn", "fn"]
 
 fig = go.Figure()
 
-fig.add_trace(go.Bar(x=x, y=[tp_confusion.iat[0], tp_confusion_second.iat[0]], marker_color='deepskyblue', name='True Positive'))
-fig.add_trace(go.Bar(x=x, y=[fp_confusion.iat[0], fp_confusion_second.iat[0]], marker_color='lightgrey', name='False Positive'))
-fig.add_trace(go.Bar(x=x, y=[(fn_confusion.iat[0]*-1), (fn_confusion_second.iat[0]*-1)], marker_color='crimson', name='False Negative'))
+fig.add_trace(go.Bar(x=x, y=[tp_confusion.iat[0], fp_confusion.iat[0], tn_confusion.iat[0], fn_confusion.iat[0]], name='True Positive'))
+
+
+# fig.add_trace(go.Bar(x=x, y=[tp_confusion_second.iat[0]], marker_color='crimson', name='True Positive'))
+# fig.add_trace(go.Bar(x=x, y=[fp_confusion_second.iat[0]], marker_color='lightgrey', name='False Positive'))
+# fig.add_trace(go.Bar(x=x, y=[tn_confusion_second.iat[0]], marker_color='deepskyblue', name='True Negative'))
+# fig.add_trace(go.Bar(x=x, y=[fn_confusion_second.iat[0]], marker_color='limegreen', name='False Negative'))
+
 
 fig.update_layout(barmode='relative', title_text='Confusion Matrix Graph')
 # fig.show()
