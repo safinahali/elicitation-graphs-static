1c9ec8c1c30d2d6f52695f1a7bbedd4d56de6a55
safinahali <safinahaali@gmail.com>
2021-11-19 17:25:45 -0500

    bars are gone, progress bar is in

--- boots/charts.js
+++ boots/charts.js
@@ -33,6 +33,10 @@ firebase.initializeApp(firebaseConfig);
 firebase.analytics();
 database = firebase.database();
 
+
+
+
+
 //data logging is set up here. need to log all the data selected
 function logdata(){
 	var uid = localStorage.getItem("uid");
@@ -157,10 +161,10 @@ function setThresholds(clicked_id){
     
     //GAURUSH: write an if statement here for your variables
     //deleting old graphs
-    document.getElementById('myChart1').remove();
-    document.getElementById('myChart2').remove();
-    document.getElementById('myChart3').remove();
-    document.getElementById('myChart4').remove();
+    // document.getElementById('myChart1').remove();
+    // document.getElementById('myChart2').remove();
+    // document.getElementById('myChart3').remove();
+    // document.getElementById('myChart4').remove();
 
     //recreating graphs
     drawCharts();
@@ -257,10 +261,10 @@ function setThresholdsA(){
     
     //GAURUSH: write an if statement here for your variables
     //deleting old graphs
-    document.getElementById('myChart1').remove();
-    document.getElementById('myChart2').remove();
-    document.getElementById('myChart3').remove();
-    document.getElementById('myChart4').remove();
+    // document.getElementById('myChart1').remove();
+    // document.getElementById('myChart2').remove();
+    // document.getElementById('myChart3').remove();
+    // document.getElementById('myChart4').remove();
 
     //recreating graphs
     drawCharts();
@@ -341,10 +345,10 @@ function setThresholdsB(){
         threshold_plot_B = (3*thresholdA+thresholdB)/4;
     }
     
-    document.getElementById('myChart1').remove();
-    document.getElementById('myChart2').remove();
-    document.getElementById('myChart3').remove();
-    document.getElementById('myChart4').remove();
+    // document.getElementById('myChart1').remove();
+    // document.getElementById('myChart2').remove();
+    // document.getElementById('myChart3').remove();
+    // document.getElementById('myChart4').remove();
 
     drawCharts();
 
@@ -360,40 +364,41 @@ function setThresholdsB(){
 
 function drawCharts(){
 
+
 	if(loadCount<1){
 		startTime = new Date();
 		loadCount++;
 	}
 
-    if (button1Count>0 || button2Count>0){
-      var myChart1 = document.createElement('canvas');
-      myChart1.setAttribute("id", "myChart1");
-      myChart1.setAttribute("width", "600");
-      myChart1.setAttribute("height", "200");
-      const chartcol1 = document.getElementById("chartcol1");
-      chartcol1.appendChild(myChart1);
+    // if (button1Count>0 || button2Count>0){
+    //   var myChart1 = document.createElement('canvas');
+    //   myChart1.setAttribute("id", "myChart1");
+    //   myChart1.setAttribute("width", "600");
+    //   myChart1.setAttribute("height", "200");
+    //   const chartcol1 = document.getElementById("chartcol1");
+    //   chartcol1.appendChild(myChart1);
 
-      var myChart2 = document.createElement('canvas');
-      myChart2.setAttribute("id", "myChart2");
+    //   var myChart2 = document.createElement('canvas');
+    //   myChart2.setAttribute("id", "myChart2");
 
-      myChart2.setAttribute("width", "600");
-      myChart2.setAttribute("height", "200");
-      chartcol1.appendChild(myChart2);
+    //   myChart2.setAttribute("width", "600");
+    //   myChart2.setAttribute("height", "200");
+    //   chartcol1.appendChild(myChart2);
 
-      var myChart3 = document.createElement('canvas');
-      myChart3.setAttribute("id", "myChart3");
+    //   var myChart3 = document.createElement('canvas');
+    //   myChart3.setAttribute("id", "myChart3");
 
-      myChart3.setAttribute("width", "600");
-      myChart3.setAttribute("height", "200");
-      const chartcol2 = document.getElementById("chartcol2");
-      chartcol2.appendChild(myChart3);
+    //   myChart3.setAttribute("width", "600");
+    //   myChart3.setAttribute("height", "200");
+    //   const chartcol2 = document.getElementById("chartcol2");
+    //   chartcol2.appendChild(myChart3);
 
-      var myChart4 = document.createElement('canvas');
-      myChart4.setAttribute("id", "myChart4");
-      myChart4.setAttribute("width", "600");
-      myChart4.setAttribute("height", "200");
-      chartcol2.appendChild(myChart4);
-    }
+    //   var myChart4 = document.createElement('canvas');
+    //   myChart4.setAttribute("id", "myChart4");
+    //   myChart4.setAttribute("width", "600");
+    //   myChart4.setAttribute("height", "200");
+    //   chartcol2.appendChild(myChart4);
+    // }
 
     for (element in dataset){
         if(dataset[element].threshold.toFixed(4) == threshold_plot_A){
@@ -421,189 +426,193 @@ function drawCharts(){
 
 //     console.log("values are" + fpA, tnA, fpB, tnB, fnA, tpA);
 
-    var ctx1 = document.getElementById('myChart1').getContext('2d');
-    myChart1 = new Chart(ctx1, {
-        type: 'bar',
-        data: {
-            // labels: ['False Positive', 'True Negative'],
-            labels: ['', ''],
-            datasets: [{
-                label: '',
-//                 data: [fpA*100, tnA*100],
-                data: [fpA, tnA],
-                backgroundColor: [
-                    'rgba(255, 113, 47, 0.5)',
-                    'rgba(54, 114, 241)'
-                ],
-                borderWidth: 1
-            }]
-        },
+//     var ctx1 = document.getElementById('myChart1').getContext('2d');
+//     myChart1 = new Chart(ctx1, {
+//         type: 'bar',
+//         data: {
+//             // labels: ['False Positive', 'True Negative'],
+//             labels: ['', ''],
+//             datasets: [{
+//                 label: '',
+// //                 data: [fpA*100, tnA*100],
+//                 data: [fpA, tnA],
+//                 backgroundColor: [
+//                     'rgba(255, 113, 47, 0.5)',
+//                     'rgba(54, 114, 241)'
+//                 ],
+//                 borderWidth: 1
+//             }]
+//         },
 
-        options: {
-        	plugins: {
-			    legend: {
-			      display: false
-			    }
-			  },
+//         options: {
+//         	plugins: {
+// 			    legend: {
+// 			      display: false
+// 			    },
 
-		    tooltips: {
-		        callbacks: {
-		           label: function(tooltipItem) {
-		                  return tooltipItem.yLabel;
-		           }
-		        }
-		    },
+// 			    datalabels: {
+// 			        color: '#36A2EB'
+// 			      }
+// 			  },
+
+// 		    tooltips: {
+// 		        callbacks: {
+// 		           label: function(tooltipItem) {
+// 		                  return tooltipItem.yLabel;
+// 		           }
+// 		        }
+// 		    },
         	
-            scales: {
-                y: {
-                    beginAtZero: true,
-                    max: 100,
-                    title: {
-                      text: 'Did not have cancer (' + String(total_n_A) + ')',
-                      display: true
-                    }
-                }
-            }
-        }
-    });
+//             scales: {
+//                 y: {
+//                     beginAtZero: true,
+//                     max: 100,
+//                     title: {
+//                       text: 'Did not have cancer (' + String(total_n_A) + ')',
+//                       display: true
+//                     }
+//                 }
+//             }
+//         }
+//     });
 
-    var ctx2 = document.getElementById('myChart2').getContext('2d');
-    myChart2 = new Chart(ctx2, {
-        type: 'bar',
-        data: {
-            labels: ['', ''],
-            labels: ['Labelled high risk (' + String(pred_p_A) + ')', 'Labelled low risk (' + String(pred_n_A) + ')'],
-            datasets: [{
-                label: '',
-//                 data: [tpA*100, fnA*100],
-                data: [tpA, fnA],
-                backgroundColor: [
-                    'rgba(255, 113, 47)',
-                    'rgba(54, 114, 241, 0.5)'
-                ],
-                borderWidth: 1
-            }]
-        },
-        options: {
+//     var ctx2 = document.getElementById('myChart2').getContext('2d');
+//     myChart2 = new Chart(ctx2, {
+//         type: 'bar',
+//         data: {
+//             labels: ['', ''],
+//             labels: ['Labelled high risk (' + String(pred_p_A) + ')', 'Labelled low risk (' + String(pred_n_A) + ')'],
+//             datasets: [{
+//                 label: '',
+// //                 data: [tpA*100, fnA*100],
+//                 data: [tpA, fnA],
+//                 backgroundColor: [
+//                     'rgba(255, 113, 47)',
+//                     'rgba(54, 114, 241, 0.5)'
+//                 ],
+//                 borderWidth: 1
+//             }]
+//         },
+//         options: {
 
-        	plugins: {
-			    legend: {
-			      display: false
-			    }
-			  },
+//         	plugins: {
+// 			    legend: {
+// 			      display: false
+// 			    }
+// 			  },
 
-		    tooltips: {
-		        callbacks: {
-		           label: function(tooltipItem) {
-		                  return tooltipItem.yLabel;
-		           }
-		        }
-		    },
+// 		    tooltips: {
+// 		        callbacks: {
+// 		           label: function(tooltipItem) {
+// 		                  return tooltipItem.yLabel;
+// 		           }
+// 		        }
+// 		    },
         	
 
-            scales: {
-                y: {
-                    beginAtZero: true,
-                    max: 100,
-                    title: {
-                      text: 'Actually had cancer (' + String(total_p_A) + ')',
-                      display: true
-                    }
-                }
-            }
-        }
-    });
+//             scales: {
+//                 y: {
+//                     beginAtZero: true,
+//                     max: 100,
+//                     title: {
+//                       text: 'Actually had cancer (' + String(total_p_A) + ')',
+//                       display: true
+//                     }
+//                 }
+//             }
+//         }
+//     });
 
-    var ctx3 = document.getElementById('myChart3').getContext('2d');
-    myChart3 = new Chart(ctx3, {
-        type: 'bar',
-        data: {
-            labels: ['', ''],
-            datasets: [{
-                label: '',
-//                 data: [fpB*100, tnB*100],
-                data: [fpB, tnB],
-                backgroundColor: [
-                    'rgba(255, 113, 47, 0.5)',
-                    'rgba(54, 114, 241)'
-                ],
-                borderWidth: 1
-            }]
-        },
-        options: {
+//     var ctx3 = document.getElementById('myChart3').getContext('2d');
+//     myChart3 = new Chart(ctx3, {
+//         type: 'bar',
+//         data: {
+//             labels: ['', ''],
+//             datasets: [{
+//                 label: '',
+// //                 data: [fpB*100, tnB*100],
+//                 data: [fpB, tnB],
+//                 backgroundColor: [
+//                     'rgba(255, 113, 47, 0.5)',
+//                     'rgba(54, 114, 241)'
+//                 ],
+//                 borderWidth: 1
+//             }]
+//         },
+//         options: {
 
-        	plugins: {
-			    legend: {
-			      display: false
-			    }
-			  },
+//         	plugins: {
+// 			    legend: {
+// 			      display: false
+// 			    }
+// 			  },
 
-		    tooltips: {
-		        callbacks: {
-		           label: function(tooltipItem) {
-		                  return tooltipItem.yLabel;
-		           }
-		        }
-		    },
+// 		    tooltips: {
+// 		        callbacks: {
+// 		           label: function(tooltipItem) {
+// 		                  return tooltipItem.yLabel;
+// 		           }
+// 		        }
+// 		    },
         	
-            scales: {
-                y: {
-                    beginAtZero: true,
-                    max: 100,
-                    title: {
-                      text: 'Did not have cancer (' + String(total_n_B) + ')',
-                      display: true
-                    }
-                }
-            }
-        }
-    });
+//             scales: {
+//                 y: {
+//                     beginAtZero: true,
+//                     max: 100,
+//                     title: {
+//                       text: 'Did not have cancer (' + String(total_n_B) + ')',
+//                       display: true
+//                     }
+//                 }
+//             }
+//         }
+//     });
 
-    var ctx4 = document.getElementById('myChart4').getContext('2d');
-    myChart4 = new Chart(ctx4, {
-        type: 'bar',
-        data: {
-//             labels: ['True Positive', 'False Negative'],
-            labels: ['Labelled high risk (' + String(pred_p_B) + ')', 'Labelled low risk (' + String(pred_n_B) + ')'],
-            datasets: [{
-                label: '',
-//                 data: [tpB*100, fnB*100],
-                data: [tpB, fnB],
-                backgroundColor: [
-                    'rgba(255, 113, 47)',
-                    'rgba(54, 114, 241, 0.5)'
-                ],
-                borderWidth: 1
-            }]
-        },
-        options: {
+//     var ctx4 = document.getElementById('myChart4').getContext('2d');
+//     myChart4 = new Chart(ctx4, {
+//         type: 'bar',
+//         data: {
+// //             labels: ['True Positive', 'False Negative'],
+//             labels: ['Labelled high risk (' + String(pred_p_B) + ')', 'Labelled low risk (' + String(pred_n_B) + ')'],
+//             datasets: [{
+//                 label: '',
+// //                 data: [tpB*100, fnB*100],
+//                 data: [tpB, fnB],
+//                 backgroundColor: [
+//                     'rgba(255, 113, 47)',
+//                     'rgba(54, 114, 241, 0.5)'
+//                 ],
+//                 borderWidth: 1
+//             }]
+//         },
+//         options: {
 
-        	plugins: {
-			    legend: {
-			      display: false
-			    }
-			  },
+//         	plugins: {
+// 			    legend: {
+// 			      display: false
+// 			    }
+// 			  },
 
-		    tooltips: {
-		        callbacks: {
-		           label: function(tooltipItem) {
-		                  return tooltipItem.yLabel;
-		           }
-		        }
-		    },
+// 		    tooltips: {
+// 		        callbacks: {
+// 		           label: function(tooltipItem) {
+// 		                  return tooltipItem.yLabel;
+// 		           }
+// 		        }
+// 		    },
         	
-            scales: {
-                y: {
-                    beginAtZero: true,
-                    max: 100,
-                    title: {
-                      text: 'Actually had cancer (' + String(total_p_B) + ')',
-                      display: true
-                    }
-                }
-            }
-        }
-    });
+//             scales: {
+//                 y: {
+//                     beginAtZero: true,
+//                     max: 100,
+//                     title: {
+//                       text: 'Actually had cancer (' + String(total_p_B) + ')',
+//                       display: true
+//                     }
+//                 }
+//             }
+//         }
+//     });
 
 
     }
--- boots/evaluation/charts.js
+++ boots/evaluation/charts.js
@@ -29,6 +29,9 @@ firebase.initializeApp(firebaseConfig);
 firebase.analytics();
 database = firebase.database();
 
+
+
+
 function logdata(){
     var uid = localStorage.getItem("uid");
 
@@ -70,10 +73,10 @@ function setThresholds(clicked_id){
     	logdata();
     }
 
-    document.getElementById('myChart1').remove();
-    document.getElementById('myChart2').remove();
-    document.getElementById('myChart3').remove();
-    document.getElementById('myChart4').remove();
+    // document.getElementById('myChart1').remove();
+    // document.getElementById('myChart2').remove();
+    // document.getElementById('myChart3').remove();
+    // document.getElementById('myChart4').remove();
 
     //recreating graphs
     drawCharts();
@@ -111,10 +114,10 @@ function setThresholdsA(){
 
     //GAURUSH: write an if statement here for your variables
     //deleting old graphs
-    document.getElementById('myChart1').remove();
-    document.getElementById('myChart2').remove();
-    document.getElementById('myChart3').remove();
-    document.getElementById('myChart4').remove();
+    // document.getElementById('myChart1').remove();
+    // document.getElementById('myChart2').remove();
+    // document.getElementById('myChart3').remove();
+    // document.getElementById('myChart4').remove();
 
     //recreating graphs
     drawCharts();
@@ -137,10 +140,10 @@ function setThresholdsB(){
     	logdata();
     }
 
-    document.getElementById('myChart1').remove();
-    document.getElementById('myChart2').remove();
-    document.getElementById('myChart3').remove();
-    document.getElementById('myChart4').remove();
+    // document.getElementById('myChart1').remove();
+    // document.getElementById('myChart2').remove();
+    // document.getElementById('myChart3').remove();
+    // document.getElementById('myChart4').remove();
 
     drawCharts();
 
@@ -162,35 +165,35 @@ function drawCharts(){
         loadCount++;
     }
 
-    if (button1Count>0 || button2Count>0){
-      var myChart1 = document.createElement('canvas');
-      myChart1.setAttribute("id", "myChart1");
-      myChart1.setAttribute("width", "600");
-      myChart1.setAttribute("height", "200");
-      const chartcol1 = document.getElementById("chartcol1");
-      chartcol1.appendChild(myChart1);
+//     if (button1Count>0 || button2Count>0){
+//       var myChart1 = document.createElement('canvas');
+//       myChart1.setAttribute("id", "myChart1");
+//       myChart1.setAttribute("width", "600");
+//       myChart1.setAttribute("height", "200");
+//       const chartcol1 = document.getElementById("chartcol1");
+//       chartcol1.appendChild(myChart1);
 
-      var myChart2 = document.createElement('canvas');
-      myChart2.setAttribute("id", "myChart2");
+//       var myChart2 = document.createElement('canvas');
+//       myChart2.setAttribute("id", "myChart2");
 
-      myChart2.setAttribute("width", "600");
-      myChart2.setAttribute("height", "200");
-      chartcol1.appendChild(myChart2);
+//       myChart2.setAttribute("width", "600");
+//       myChart2.setAttribute("height", "200");
+//       chartcol1.appendChild(myChart2);
 
-      var myChart3 = document.createElement('canvas');
-      myChart3.setAttribute("id", "myChart3");
+//       var myChart3 = document.createElement('canvas');
+//       myChart3.setAttribute("id", "myChart3");
 
-      myChart3.setAttribute("width", "600");
-      myChart3.setAttribute("height", "200");
-      const chartcol2 = document.getElementById("chartcol2");
-      chartcol2.appendChild(myChart3);
+//       myChart3.setAttribute("width", "600");
+//       myChart3.setAttribute("height", "200");
+//       const chartcol2 = document.getElementById("chartcol2");
+//       chartcol2.appendChild(myChart3);
 
-      var myChart4 = document.createElement('canvas');
-      myChart4.setAttribute("id", "myChart4");
-      myChart4.setAttribute("width", "600");
-      myChart4.setAttribute("height", "200");
-      chartcol2.appendChild(myChart4);
-    }
+//       var myChart4 = document.createElement('canvas');
+//       myChart4.setAttribute("id", "myChart4");
+//       myChart4.setAttribute("width", "600");
+//       myChart4.setAttribute("height", "200");
+//       chartcol2.appendChild(myChart4);
+//     }
 
     for (element in dataset){
         if(dataset[element].threshold.toFixed(4) == thresholdA){
@@ -218,128 +221,128 @@ function drawCharts(){
 
     console.log("values are" + fpA, tnA, fpB, tnB, fnA, tpA);
 
-    var ctx1 = document.getElementById('myChart1').getContext('2d');
-    myChart1 = new Chart(ctx1, {
-        type: 'bar',
-        data: {
-//             labels: ['False Positive', 'True Negative'],
-            labels: ['', ''],
-            datasets: [{
-                label: '',
-//                 data: [fpA*100, tnA*100],
-                data: [fpA, tnA],
-                backgroundColor: [
-                    'rgba(255, 113, 47, 0.5)',
-                    'rgba(54, 114, 241)'
-                ],
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true,
-                    max: 100,
-                    title: {
-                      text: 'Did not have cancer (' + String(total_n_A) + ')',
-                      display: true
-                    }
-                }
-            }
-        }
-    });
+//     var ctx1 = document.getElementById('myChart1').getContext('2d');
+//     myChart1 = new Chart(ctx1, {
+//         type: 'bar',
+//         data: {
+// //             labels: ['False Positive', 'True Negative'],
+//             labels: ['', ''],
+//             datasets: [{
+//                 label: '',
+// //                 data: [fpA*100, tnA*100],
+//                 data: [fpA, tnA],
+//                 backgroundColor: [
+//                     'rgba(255, 113, 47, 0.5)',
+//                     'rgba(54, 114, 241)'
+//                 ],
+//                 borderWidth: 1
+//             }]
+//         },
+//         options: {
+//             scales: {
+//                 y: {
+//                     beginAtZero: true,
+//                     max: 100,
+//                     title: {
+//                       text: 'Did not have cancer (' + String(total_n_A) + ')',
+//                       display: true
+//                     }
+//                 }
+//             }
+//         }
+//     });
 
-    var ctx2 = document.getElementById('myChart2').getContext('2d');
-    myChart2 = new Chart(ctx2, {
-        type: 'bar',
-        data: {
-            labels: ['', ''],
-            labels: ['Labelled high risk (' + String(pred_p_A) + ')', 'Labelled low risk (' + String(pred_n_A) + ')'],
-            datasets: [{
-                label: '',
-//                 data: [tpA*100, fnA*100],
-                data: [tpA, fnA],
-                backgroundColor: [
-                    'rgba(255, 113, 47)',
-                    'rgba(54, 114, 241, 0.5)'
-                ],
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true,
-                    max: 100,
-                    title: {
-                      text: 'Actually had cancer (' + String(total_p_A) + ')',
-                      display: true
-                    }
-                }
-            }
-        }
-    });
+//     var ctx2 = document.getElementById('myChart2').getContext('2d');
+//     myChart2 = new Chart(ctx2, {
+//         type: 'bar',
+//         data: {
+//             labels: ['', ''],
+//             labels: ['Labelled high risk (' + String(pred_p_A) + ')', 'Labelled low risk (' + String(pred_n_A) + ')'],
+//             datasets: [{
+//                 label: '',
+// //                 data: [tpA*100, fnA*100],
+//                 data: [tpA, fnA],
+//                 backgroundColor: [
+//                     'rgba(255, 113, 47)',
+//                     'rgba(54, 114, 241, 0.5)'
+//                 ],
+//                 borderWidth: 1
+//             }]
+//         },
+//         options: {
+//             scales: {
+//                 y: {
+//                     beginAtZero: true,
+//                     max: 100,
+//                     title: {
+//                       text: 'Actually had cancer (' + String(total_p_A) + ')',
+//                       display: true
+//                     }
+//                 }
+//             }
+//         }
+//     });
 
-    var ctx3 = document.getElementById('myChart3').getContext('2d');
-    myChart3 = new Chart(ctx3, {
-        type: 'bar',
-        data: {
-            labels: ['', ''],
-            datasets: [{
-                label: '',
-//                 data: [fpB*100, tnB*100],
-                data: [fpB, tnB],
-                backgroundColor: [
-                    'rgba(255, 113, 47, 0.5)',
-                    'rgba(54, 114, 241)'
-                ],
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true,
-                    max: 100,
-                    title: {
-                      text: 'Did not have cancer (' + String(total_n_B) + ')',
-                      display: true
-                    }
-                }
-            }
-        }
-    });
+//     var ctx3 = document.getElementById('myChart3').getContext('2d');
+//     myChart3 = new Chart(ctx3, {
+//         type: 'bar',
+//         data: {
+//             labels: ['', ''],
+//             datasets: [{
+//                 label: '',
+// //                 data: [fpB*100, tnB*100],
+//                 data: [fpB, tnB],
+//                 backgroundColor: [
+//                     'rgba(255, 113, 47, 0.5)',
+//                     'rgba(54, 114, 241)'
+//                 ],
+//                 borderWidth: 1
+//             }]
+//         },
+//         options: {
+//             scales: {
+//                 y: {
+//                     beginAtZero: true,
+//                     max: 100,
+//                     title: {
+//                       text: 'Did not have cancer (' + String(total_n_B) + ')',
+//                       display: true
+//                     }
+//                 }
+//             }
+//         }
+//     });
 
-    var ctx4 = document.getElementById('myChart4').getContext('2d');
-    myChart4 = new Chart(ctx4, {
-        type: 'bar',
-        data: {
-//             labels: ['True Positive', 'False Negative'],
-            labels: ['Labelled high risk (' + String(pred_p_B) + ')', 'Labelled low risk (' + String(pred_n_B) + ')'],
-            datasets: [{
-                label: '',
-//                 data: [tpB*100, fnB*100],
-                data: [tpB, fnB],
-                backgroundColor: [
-                    'rgba(255, 113, 47)',
-                    'rgba(54, 114, 241, 0.5)'
-                ],
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                y: {
-                    beginAtZero: true,
-                    max: 100,
-                    title: {
-                      text: 'Actually had cancer (' + String(total_p_B) + ')',
-                      display: true
-                    }
-                }
-            }
-        }
-    });
+//     var ctx4 = document.getElementById('myChart4').getContext('2d');
+//     myChart4 = new Chart(ctx4, {
+//         type: 'bar',
+//         data: {
+// //             labels: ['True Positive', 'False Negative'],
+//             labels: ['Labelled high risk (' + String(pred_p_B) + ')', 'Labelled low risk (' + String(pred_n_B) + ')'],
+//             datasets: [{
+//                 label: '',
+// //                 data: [tpB*100, fnB*100],
+//                 data: [tpB, fnB],
+//                 backgroundColor: [
+//                     'rgba(255, 113, 47)',
+//                     'rgba(54, 114, 241, 0.5)'
+//                 ],
+//                 borderWidth: 1
+//             }]
+//         },
+//         options: {
+//             scales: {
+//                 y: {
+//                     beginAtZero: true,
+//                     max: 100,
+//                     title: {
+//                       text: 'Actually had cancer (' + String(total_p_B) + ')',
+//                       display: true
+//                     }
+//                 }
+//             }
+//         }
+//     });
 
 
     }
